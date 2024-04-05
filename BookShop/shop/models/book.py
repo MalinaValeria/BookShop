78efb68c2aed48b659616800e55f5d856d1cc7bd
Valeria Malina <wollt1362@gmail.com>
2024-04-05 15:26:15 +0300

    Fix migrations

--- BookShop/shop/models/book.py
+++ BookShop/shop/models/book.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=200, verbose_name='Название книги')
-    authors = models.ManyToManyField('shop.Author', through='shop.BookAuthor', related_name='books', verbose_name='Авторы книги')
+    authors = models.ManyToManyField(related_name='books', through='shop.BookAuthor', to='shop.Author',
+                                     verbose_name='Авторы')
     category = models.ForeignKey('shop.Category', on_delete=models.CASCADE)
 
     def __str__(self):
