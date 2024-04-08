d22fb0def36c7f177e8cf4020f1a1ffabba106b2
Valeria Malina <wollt1362@gmail.com>
2024-04-08 19:56:52 +0300

    Add book covers. Header, part of main page

--- BookShop/shop/views/main.py
+++ BookShop/shop/views/main.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
 
+from shop.models import BookInfo
+
 
 class Main(TemplateView):
     template_name = 'main.html'
-    model = 'shop.Book'
+    model = BookInfo
     context_object_name = 'books'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['new_books'] = self.model.objects.all().order_by('-publication_year')[:18]
+        return context
