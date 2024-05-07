2dd130951b5ba0070e16d7a7ebe991e6fb685075
Valeria Malina <wollt1362@gmail.com>
2024-05-07 15:06:19 +0300

    The basis of the main page

--- BookShop/shop/views/main.py
+++ BookShop/shop/views/main.py
@@ -11,4 +11,7 @@ class Main(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['new_books'] = self.model.objects.all().order_by('-publication_year')[:18]
+        context['popular_books'] = self.model.objects.all().order_by('-circulation')[:18]
+        context['foreign_books'] = self.model.objects.filter(book__category__parent_id=2).order_by('-publication_year')[:18]
+        context['book_count'] = (':6', '6:12', '12:')
         return context
